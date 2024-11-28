029745fe7aa74f9088facf83a33f7995f45c76ba
Piper Maxine Baker <github@piper.community>
2024-11-28 09:22:45 -0500

    mimic human/machine formatting behavior of ag

--- todos.sh
+++ todos.sh
@@ -1,15 +1,51 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+RESET='\033[0m' 
+
+branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+if [ $? -ne 0 ]; then
+    echo Must be within a git repository. Exiting. >&2
+    exit 1
+fi    
 
 commits=$(git cherry -v main | grep '^+' | cut -d' ' -f2)
 earliest=$(echo $commits | tail -n1)
 latest=$(echo $commits | head -n1)
 
-for file in $(git diff --name-only "${earliest}^" $latest); do
-    git diff -U0 "${earliest}^" $latest -- $file | \
-        grep -v "^+++" \
-        | grep "^+" \
-        | grep -e TODO -e FIXME \
-        | sed 's/^+//' \
-        | awk  '{print "'$file'" ": " $1}' \
+human_format() {
+    local file=$1
+    local lines_with_numbers=$2
 
+    echo -e "$GREEN${file}$RESET"
+    while read -r line_with_number; do
+        number=$(echo $line_with_number | cut -d: -f1)
+        line=$(echo $line_with_number | cut -d: -f2-)
+        echo -e "$YELLOW$number$RESET:$line"
+    done <<< "$lines_with_numbers"        
+}
+
+machine_format() {
+    local file=$1
+    local lines_with_numbers=$2
+
+    while read -r line_with_number; do
+        echo "${file}:${line_with_number}"
+    done <<< "$lines_with_numbers"    
+}
+
+for file in $(git diff --name-only -S"TODO" "${earliest}^" $latest); do
+    lines_with_numbers=$(git diff -U999999 "${earliest}^" $latest -- $file \
+        | tail -n+6 \
+        | grep -n '^+' \
+        | grep "TODO" \
+        | sed 's/^\([0-9]\):+/\1:/')
+
+    if [[ -t 1 ]]; then
+        human_format "${file}" "${lines_with_numbers}"
+    else
+        machine_format "${file}" "${lines_with_numbers}"
+    fi
 done
