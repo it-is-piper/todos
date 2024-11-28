cef96c1a864e1d6c24cd914a5de0107a3450893b
Piper Maxine Baker <github@piper.community>
2024-11-28 09:58:47 -0500

    working ndjson output

--- todos.sh
+++ todos.sh
@@ -9,7 +9,7 @@ branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
 if [ $? -ne 0 ]; then
     echo Must be within a git repository. Exiting. >&2
     exit 1
-fi    
+fi
 
 commits=$(git cherry -v main | grep '^+' | cut -d' ' -f2)
 earliest=$(echo $commits | cut -d' ' -f1)
@@ -33,7 +33,18 @@ machine_format() {
 
     while read -r line_with_number; do
         echo "${file}:${line_with_number}"
-    done <<< "$lines_with_numbers"    
+    done <<< "$lines_with_numbers"
+}
+
+json_format() {
+    local file=$1
+    local lines_with_numbers=$2
+
+    while read -r line_with_number; do
+        number=$(echo $line_with_number | cut -d: -f1)
+        line=$(echo $line_with_number | cut -d: -f2-)
+        jq -n --arg file $file --arg number $number --arg line "$line" '{ "file": $file, "number": $number, "line": $line }'
+    done <<< "$lines_with_numbers"
 }
 
 for file in $(git diff --name-only -S"TODO" "${earliest}^" $latest); do
@@ -43,9 +54,11 @@ for file in $(git diff --name-only -S"TODO" "${earliest}^" $latest); do
         | grep "TODO" \
         | sed -E 's/^([0-9]+):\+/\1:/')
 
-    if [[ -t 1 ]]; then
-        human_format "${file}" "${lines_with_numbers}"
-    else
-        machine_format "${file}" "${lines_with_numbers}"
-    fi
+    json_format "${file}" "${lines_with_numbers}"
+    
+    # if [[ -t 1 ]]; then
+    #     human_format "${file}" "${lines_with_numbers}"
+    # else
+    #     machine_format "${file}" "${lines_with_numbers}"
+    # fi
 done
