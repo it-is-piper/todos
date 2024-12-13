ae6a18c2ae87f107ec459fa1da76d396517d437c
Piper Maxine Baker <github@piper.community>
2024-12-13 16:35:41 -0500

    (wip) Got JSON format working with one bug!
    Bug is documented in todos

--- todos.py
+++ todos.py
@@ -1,27 +1,161 @@
 import os
+import json
 from git import Git
-from typing import List
+from typing import List, Optional, Any, Tuple
+from dataclasses import dataclass
+from argparse import ArgumentParser
+from enum import Enum
+
+
+def _remove_nones(values: List[Optional[Any]]) -> List[Any]:
+    """Remove all None values from a list.
+    """
+    return [v for v in values if v is not None]
+
+
+class Format(str, Enum):
+    HUMAN = "human"
+    MACHINE = "machine"
+    JSON = "json"
+
+
+@dataclass
+class Line:
+    path: str
+    number: int
+    text: str
+
+    def to_dict(self):
+        return {
+            "file": self.path,
+            "line": self.number,
+            "text": self.text
+        }
 
 
 class Todos:
     base_branch: str
-    g: git.Git
+    key: str
+    cached: bool
+    unstaged: bool
+    g: Git
 
-    def __init__(self, base_branch: str = "main"):
+    def __init__(self,
+                 base_branch: str = "main",
+                 key: str = "TODO",
+                 cached: bool = False,
+                 unstaged: bool = False):
         self.base_branch = base_branch
+        self.key = key
+        self.cached = cached
+        self.unstaged = unstaged
         self.g = Git(os.getcwd())
 
-    def _commits(self) -> List[str]:
-        cherries = self.g.cherry(["-v", self.base_branch])
-        
-    def _earliest(self, commits: List[str]) -> str:
-        return 
+        if self.cached and self.unstaged:
+            # TODO use a more specific exception type
+            raise Exception(
+                "cached and unstaged are mutually exclusive options")
 
-    def _latest(self) -> str:
+    def _commits(self) -> List[str]:
+        # cherry outputs each commit that's in one branch but not the other
+        cherry_output = self.g.cherry(["-v", self.base_branch])
+        all_cherries = cherry_output.split("\n")
+
+        # git diff appends a "+ " in front of commits in the current branch that are not
+        # in the base branch"
+        added_cherries = [c for c in all_cherries if c[:2] == "+ "]
+
+        # there's a space between the "+" and the commit hash
+        hashes = [c.split(" ")[1] for c in added_cherries]
+        return hashes
+
+    def _files(self, left: str, right: Optional[str] = None) -> List[str]:
+        # Get names of files that changed in this commit range that have the key in them
+        flags = ["--name-only", f"-S\"{self.key}\""]
+        if self.cached:
+            flags.append("--cached")
+        diff_args = _remove_nones([*flags, left, right])
+
+        # TODO For some reason this returns an empty change set when left is 1 commit before
+        # right, but the verbatim command succeeds when run from a shell.
+        diff_output = self.g.diff(diff_args)
+        files_with_keys = [line for line in diff_output.split("\n") if line != '']
+        return files_with_keys
+
+    def _files_and_lines(self, left: str, right: Optional[str]) -> Tuple[List[str], List[Line]]:
+        flags = ["-U999999"]
+        if self.cached:
+            flags.append("--cached")
+
+        paths = self._files(left, right)
+        result: List[Line] = []
+
+        for path in paths:
+            diff_args = _remove_nones([*flags, left, right, "--", path])
+            diff_output = self.g.diff(diff_args)
+
+            # ignore the first 6 lines, which display metadata
+            diff_lines = diff_output.split("\n")[6:]
+
+            # we need to figure out what the line number is for each added line
+            lines_with_key = [
+                Line(path=path, number=number, text=text)
+                for number, text in enumerate(diff_lines)
+                if text[:1] == "+" and self.key in text
+            ]
+            result += lines_with_key
+
+        return paths, result
+
+    def files_and_lines(self) -> Tuple[List[str], List[Line]]:
+        """Return a list of `Line` objects for added line containing `self.key`.
+        """
+        commits = self._commits()
+
+        # We want to diff with the commit *before* the first one we added in our branch
+        left = f"{commits[0]}^"
+
+        # The right side of the diff should be empty unless we're only looking at
+        # committed files
+        right = None if self.cached or self.unstaged else commits[-1]
+
+        return self._files_and_lines(left, right)
+
+
+def human_format(files: List[str], lines: List[Line]):
+    print("human format is unsupported")
+
+
+def machine_format(files: List[str], lines: List[Line]):
+    print("machine format is unsupported")
+
+
+def json_format(files: List[str], lines: List[Line]):
+    objects = [line.to_dict() for line in lines]
+    output = json.dumps(objects, indent=2)
+    print(output)
 
 
 if __name__ == "__main__":
-    print("[TODO] things are going to happen here")
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--format",
+        choices=[Format.HUMAN, Format.MACHINE, Format.JSON],
+        default=Format.JSON
+    )
+    # TODO add validation that these aren't passed together
+    # TODO also can't remember how to set things as flags
+    # parser.add_argument("--cached", default=False)
+    # parser.add_argument("--unstaged", default=False))
+    args = parser.parse_args()
 
-    # TODO we're just going to start with default behavior of comparing the current tree
-    # to main
+    # TODO switch back to using args once I have them
+    todos = Todos(unstaged=True, cached=False)
+    files, lines = todos.files_and_lines()
+
+    if args.format == Format.HUMAN:
+        human_format(files, lines)
+    elif args.format == Format.MACHINE:
+        machine_format(files, lines)
+    elif args.format == Format.JSON:
+        json_format(files, lines)
